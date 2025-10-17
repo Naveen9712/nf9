ddb6f815cf742ccc7f60a055a02cd95295e13fef
Naveen9712 <kandakurthinaveen97@gmail.com>
2025-10-17 21:48:59 +0530

    removed button

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -126,36 +126,6 @@ export default function Banner({ isAnimated, onSmoothScroll }: BannerProps) {
               </svg>
             </span>
           </a>
-          <a href="#services" className="button button-secondary" onClick={(e) => onSmoothScroll(e, '#services')}>
-            Explore Services
-            <span className="button__icon-wrapper">
-              <svg
-                viewBox="0 0 14 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="button__icon-svg"
-                width="10"
-              >
-                <path
-                  d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-
-              <svg
-                viewBox="0 0 14 15"
-                fill="none"
-                width="10"
-                xmlns="http://www.w3.org/2000/svg"
-                className="button__icon-svg button__icon-svg--copy"
-              >
-                <path
-                  d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </span>
-          </a>
         </div>
       </div>
     </section>
