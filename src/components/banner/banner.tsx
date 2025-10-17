62b0eedd82d791280b3d4fd61e2264677e9c4d23
Naveen9712 <kandakurthinaveen97@gmail.com>
2025-10-17 21:16:23 +0530

    button updated

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -96,11 +96,65 @@ export default function Banner({ isAnimated, onSmoothScroll }: BannerProps) {
         </div>
 
         <div className={`banner-buttons ${isAnimated ? 'animate-in animate-delay-3' : ''}`}>
-          <a href="#contact" className="btn btn-primary" onClick={(e) => onSmoothScroll(e, '#contact')}>
-            <span>Start Your Project</span>
+          <a href="#contact" className="button button-primary" onClick={(e) => onSmoothScroll(e, '#contact')}>
+            Start Your Project
+            <span className="button__icon-wrapper">
+              <svg
+                viewBox="0 0 14 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="button__icon-svg"
+                width="10"
+              >
+                <path
+                  d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+
+              <svg
+                viewBox="0 0 14 15"
+                fill="none"
+                width="10"
+                xmlns="http://www.w3.org/2000/svg"
+                className="button__icon-svg button__icon-svg--copy"
+              >
+                <path
+                  d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </span>
           </a>
-          <a href="#services" className="btn btn-secondary" onClick={(e) => onSmoothScroll(e, '#services')}>
-            <span>Explore Services</span>
+          <a href="#services" className="button button-secondary" onClick={(e) => onSmoothScroll(e, '#services')}>
+            Explore Services
+            <span className="button__icon-wrapper">
+              <svg
+                viewBox="0 0 14 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="button__icon-svg"
+                width="10"
+              >
+                <path
+                  d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+
+              <svg
+                viewBox="0 0 14 15"
+                fill="none"
+                width="10"
+                xmlns="http://www.w3.org/2000/svg"
+                className="button__icon-svg button__icon-svg--copy"
+              >
+                <path
+                  d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </span>
           </a>
         </div>
       </div>
