ee5c96a6cc180f8e3815cb37bc71c9600cd4c7e3
Anil Bai <anilbai397@gmail.com>
2025-12-28 21:23:17 +0530

    update

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
             preload="auto"
           >
             <source
-              src="https://res.cloudinary.com/dzwm5v9gy/video/upload/v1766932484/WhatsApp_Video_2025-12-15_at_19.22.36_s5yts1.mp4"
+              src="https://res.cloudinary.com/dzwm5v9gy/video/upload/v1766935871/NF9_Banner_Video_mabxxy.mp4"
               type="video/mp4"
             />
           </video>
@@ -25,7 +25,6 @@ const Hero = () => {
         {/* Overlay Content */}
         <div className="inner">
           <div className="heroText">
-            <h1 className="heroTitle">NF9</h1>
             <p className="heroSubtitle">IDEAS. ENGINEERED.</p>
           </div>
         </div>
