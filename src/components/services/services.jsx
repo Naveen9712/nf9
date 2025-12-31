9935c3e36c962a3aece2af98cc5df139e839f9e3
Anil Bai <anilbai397@gmail.com>
2025-12-31 20:11:31 +0530

    update

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,86 +1,60 @@
 import "./services.css";
-import { useRef } from "react";
 
 const services = [
   {
     title: "UI/UX Design",
-    image: "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1765214774/1duOVcilpl3cJsg3KzVgMJtYicM_vc81tz.avif",
+    image:
+      "https://framerusercontent.com/images/XZ6mh9GNTG6AuAVejsGZeSIxzUQ.jpeg",
     gif: "https://media.giphy.com/media/3o7aD2saalBwwftBIY/giphy.gif",
   },
   {
     title: "Website Design & Development",
-    image: "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1761918540/main-sample.png",
+    image:
+      "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1761918540/main-sample.png",
     gif: "https://media.giphy.com/media/l0HlNaQ6gWfllcjDO/giphy.gif",
   },
   {
     title: "eCommerce & SaaS Development",
-    image: "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1761918539/cld-sample-4.jpg",
+    image:
+      "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1761918539/cld-sample-4.jpg",
     gif: "https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif",
   },
   {
     title: "Application Development",
-    image: "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1761918540/main-sample.png",
+    image:
+      "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1761918540/main-sample.png",
     gif: "https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif",
   },
   {
     title: "Cloud, DevOps & Server Management",
-    image: "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1765214774/1duOVcilpl3cJsg3KzVgMJtYicM_vc81tz.avif",
-    gif: "https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
+    image:
+      "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1761918539/cld-sample-4.jpg",
+    gif: "https://media.giphy.com/media/l41lFw057lAJQMwg0/giphy.gif",
   },
   {
     title: "Branding, SEO & Digital Marketing",
-    image: "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1761918539/cld-sample-4.jpg",
-    gif: "https://media.giphy.com/media/l41lFw057lAJQMwg0/giphy.gif",
+    image:
+      "https://res.cloudinary.com/dzwm5v9gy/image/upload/v1761918540/main-sample.png",
+    gif: "https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif",
   },
 ];
 
 export default function Services() {
-  const gifRefs = useRef([]);
-
-  const handleMouseMove = (e, index) => {
-    const card = e.currentTarget.getBoundingClientRect();
-    const gif = gifRefs.current[index];
-    if (!gif) return;
-
-    const x = e.clientX - card.left;
-    const y = e.clientY - card.top;
-
-    const rotateX = ((y - card.height / 2) / card.height) * -12;
-    const rotateY = ((x - card.width / 2) / card.width) * 12;
-
-    gif.style.transform = `
-      translate(${x - card.width / 2}px, ${y - card.height / 2}px)
-      rotateX(${rotateX}deg)
-      rotateY(${rotateY}deg)
-      scale(1)
-    `;
-  };
-
   return (
     <section className="services">
+      {/* Header */}
       <div className="services-header">
-        <h2>Our Services</h2>
+        <h2>Services</h2>
       </div>
 
+      {/* Grid */}
       <div className="services-grid">
         {services.map((item, index) => (
-          <div
-            className="service-card"
-            key={index}
-            onMouseMove={(e) => handleMouseMove(e, index)}
-          >
-            <div className="service-image">
-              <img src={item.image} alt={item.title} className="static-img" />
+          <div className="service-card" key={index}>
+            <div className="service-media">
+              <img src={item.image} alt={item.title} className="media static" />
+              <img src={item.gif} alt="" className="media gif" />
             </div>
-
-            <img
-              ref={(el) => (gifRefs.current[index] = el)}
-              src={item.gif}
-              alt=""
-              className="hover-gif"
-            />
-
-            <span className="view-badge">VIEW</span>
 
             <div className="service-info">
               <h3>{item.title}</h3>
@@ -89,8 +63,17 @@ export default function Services() {
         ))}
       </div>
 
-      <div className="services-button">
-        <button className="services-btn">See what we do</button>
+      {/* GLOBAL CTA */}
+      <div className="services-cta-wrap">
+        <a href="/contact" className="framer-cta">
+          <span className="cta-label">GET STARTED TODAY</span>
+          <span className="cta-line"></span>
+          <span className="cta-arrow">
+            <svg viewBox="0 0 24 24" aria-hidden="true">
+              <path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z" />
+            </svg>
+          </span>
+        </a>
       </div>
     </section>
   );
